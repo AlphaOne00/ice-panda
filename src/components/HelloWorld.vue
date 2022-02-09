7f881cf960c77a45230af82fe91dfc46ff53ade0
ZhangGQ <2366043513@qq.com>
2022-02-09 23:54:04 +0800

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,60 +1,122 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <canvas id="three"></canvas>
   </div>
 </template>
 
 <script>
+import * as THREE from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  mounted() {
+    this.initThree()
+  },
+  methods: {
+    initThree() {
+      const scene = new THREE.Scene()
+      scene.background = new THREE.Color('#eee')
+      scene.fog = new THREE.Fog('#eee', 20, 100)
+
+      const canvas = document.querySelector('#three')
+      const renderer = new THREE.WebGLRenderer({ canvas, antialias: true })
+      renderer.shadowMap.enabled = true
+
+      const camera = new THREE.PerspectiveCamera(
+        50,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+      )
+      camera.position.z = 10
+
+      const gltfLoader = new GLTFLoader()
+      gltfLoader.load('/model/dwendwen.gltf', (gltf) => {
+        let model = gltf.scene
+        // 遍历模型每部分
+        model.traverse((child) => {
+          //将图片作为纹理加载
+          let explosionTexture = new THREE.TextureLoader().load(
+            '/model/env.jpg'
+          )
+          if(child.name=="outer"||child.name=="mask"){
+              child.material.envMap = explosionTexture;
+              child.material.envMap.mapping = THREE.EquirectangularReflectionMapping;
+              child.material.envMapIntensity=2;
+            }else if(child.name=="body"){
+              const map = child.material.map;
+              child.material = new THREE.MeshToonMaterial({map:map});
+            }
+        })
+        scene.add(model)
+      })  
+
+      const hemLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.6)
+      hemLight.position.set(0, 48, 0)
+      scene.add(hemLight)
+
+      const dirLight = new THREE.DirectionalLight(0xffffff, 0.6)
+      //光源等位置
+      dirLight.position.set(-10, 8, -5)
+      //可以产生阴影
+      dirLight.castShadow = true
+      dirLight.shadow.mapSize = new THREE.Vector2(1024, 1024)
+      scene.add(dirLight)
+
+      let floorGeometry = new THREE.PlaneGeometry(8000, 8000)
+      let floorMaterial = new THREE.MeshPhongMaterial({
+        color: 0x857ebb,
+        shininess: 0,
+      })
+
+      let floor = new THREE.Mesh(floorGeometry, floorMaterial)
+      floor.rotation.x = -0.5 * Math.PI
+      floor.receiveShadow = true
+      floor.position.y = -0.001
+      // scene.add(floor)
+
+      const controls = new OrbitControls(camera, renderer.domElement)
+      controls.enableDamping = true
+
+      function animate() {
+        controls.update()
+        renderer.render(scene, camera)
+        requestAnimationFrame(animate)
+
+        if (resizeRendererToDisplaySize(renderer)) {
+          const canvas = renderer.domElement
+          camera.aspect = canvas.clientWidth / canvas.clientHeight
+          camera.updateProjectionMatrix()
+        }
+      }
+      animate()
+
+      function resizeRendererToDisplaySize(renderer) {
+        const canvas = renderer.domElement
+        var width = window.innerWidth
+        var height = window.innerHeight
+        var canvasPixelWidth = canvas.width / window.devicePixelRatio
+        var canvasPixelHeight = canvas.height / window.devicePixelRatio
+
+        const needResize =
+          canvasPixelWidth !== width || canvasPixelHeight !== height
+        if (needResize) {
+          renderer.setSize(width, height, false)
+        }
+        return needResize
+      }
+    },
+  },
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style>
+#three{
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
 }
 </style>
